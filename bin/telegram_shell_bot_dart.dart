c2f29cf4871a83a22071eda8e42ccdb2cb822841
￴ <>
2022-12-04 01:32:59 +0700

    first commit

---
+++ bin/telegram_shell_bot_dart.dart
@@ -0,0 +1,5 @@
+import 'package:telegram_shell_bot_dart/telegram_shell_bot_dart.dart' as telegram_shell_bot_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${telegram_shell_bot_dart.calculate()}!');
+}
---
+++ test/telegram_shell_bot_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:telegram_shell_bot_dart/telegram_shell_bot_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
